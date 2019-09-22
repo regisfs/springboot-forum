a33562c7a79ddd991d07b6cc403c517a7309df86
regisfs <regisfs@gmail.com>
2019-09-22 18:03:40 -0300

    acrescentando valores default na paginação da API

--- src/main/java/br/com/sodescomplicado/forum/controller/TopicosController.java
+++ src/main/java/br/com/sodescomplicado/forum/controller/TopicosController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -43,7 +44,7 @@ public class TopicosController {
 
 	@GetMapping
 	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
-			Pageable paginacao) {
+			@PageableDefault(sort="id", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao) {
 		
 		if (nomeCurso == null) {
 
